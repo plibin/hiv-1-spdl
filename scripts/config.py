dba95f135db05d867512c9836b01a7d251d0f32c
Pieter Libin <pieter.libin@vub.be>
2025-10-25 22:10:07 +0200

    updates

--- scripts/config.py
+++ scripts/config.py
@@ -2,4 +2,6 @@ def proteins():
     return ["PR", "IN", "RT"]
 
 def algorithms():
-    return ["AlphaFold2", "AlphaFold3", "ESM3-Open", "ESM3-Large", "ESMFold", "Ember3D"]
+    #return ["AlphaFold2", "AlphaFold3", "ESM3-Open", "ESM3-Large", "ESMFold", "Ember3D"]
+     return ["AlphaFold2", "AlphaFold3", "ESMFold"]
+ #TODO: the other algo's only seem to have half of PROTEASE, compared to the PDB?
