195dbb08e99b1fd5bce61cef2bbd879ee9ae6345
Pieter Libin <pieter.libin@vub.be>
2025-10-26 20:28:09 +0100

    add tm

---
+++ scripts/tm.py
@@ -0,0 +1,28 @@
+import numpy as np
+from math import sqrt
+
+def _dist(r_ref: Residue.Residue, r_pred: Residue.Residue) -> float:
+    diff = r_ref.get_coord() - r_pred.get_coord()
+    #Euclidean norm
+    return float(np.sqrt(np.dot(diff, diff)))
+
+def global_tm(start: int, end: int,
+              ref_chain: Chain.Chain,
+              pred_chain: Chain.Chain,
+              L_N: int) -> float:
+    d_i_list = core.stat_per_residue(start, end, ref_chain, pred_chain, _dist)
+
+    # As specified in the original TM-score paper: 10.1002/prot.20264,
+    # d_i  is the distance between the i-th pair of aligned residues,
+    # d_i_list is the list of distances over all considered residues,
+    # d0 is a scale to normalize the match difference,
+    # L_N is the length of the native structure,
+    # L_T is the length of the aligned residues to the template structure,
+    # (here that is the length of d_i_list).
+    d_0 = 1.24 * (L_N - 15)**(1.0/3.0) - 1.8
+    if d_0 < 0.5:
+        d_0 = 0.5
+
+    terms = [1.0 / (1.0 + (d_i / d_0)**2) for d_i in d_i_list]
+    
+    return sum(terms)/float(L_N)
