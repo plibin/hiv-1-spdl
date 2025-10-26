92c817b58f1110ab780c2778bc02230cef1c9246
Pieter Libin <pieter.libin@vub.be>
2025-10-26 20:27:36 +0100

    many CLI updates

--- scripts/cli-pos.py
+++ scripts/cli-pos.py
@@ -4,21 +4,16 @@ import pandas as pd
 import config as config
 import myio as io
 import rmsd
+import plddt
+import cli
 from pathlib import Path
-
-def parse_range(s):
-    try:
-        start, end = map(int, s.split('-'))
-        return start, end
-    except Exception:
-        raise argparse.ArgumentTypeError("Range must be in START-END format (e.g. 5-10)")
 
 def main():
     parser = argparse.ArgumentParser(description="CLI for positional statistics.")
     parser.add_argument("--base-path", "-b", required=True)
     parser.add_argument("--protein", "-p",  required=True)
     parser.add_argument("--stat", "-s", choices=["rmsd","plddt"], required=True)
-    parser.add_argument("--range", type=parse_range, required=True)
+    parser.add_argument("--range", type=cli.parse_range, required=True)
 
     args = parser.parse_args()
 
@@ -27,23 +22,36 @@ def main():
     refs = io.load_refs(base_path, args.protein)
 
     start, end = args.range
-    
+
+    dfs = []
     for algorithm in config.algorithms():
         print("algo:" + algorithm)
         preds = io.load_preds(refs, base_path, args.protein, algorithm)
         for ref in refs.keys():
-            print("ref:" + ref)
             r = refs[ref]
             p = preds[ref]
-            if args.stat == "rmsd":
-                r_chain = io._first_chain(io._first_model(r))
-                p_chain = io._first_chain(io._first_model(p))
-                df = rmsd.per_residue_rmsd(start, end, r, p)
-                #TODO: add algo + structure name to the df
-                
 
-    # Export DataFrame as CSV to stdout
-    #df.to_csv(sys.stdout, index=False)
+            r_chain = io._first_chain(io._first_model(r))
+            p_chain = io._first_chain(io._first_model(p))
+
+            df = None
+            if args.stat == "rmsd":
+                pos_to_rmsd = rmsd.per_residue_rmsd(start, end, r_chain, p_chain)
+
+                rows = [{"pos": pos + 1, "RMSD": rmsd} for pos, rmsd in pos_to_rmsd.items()]
+                df = pd.DataFrame(rows)
+            elif args.stat == "plddt":
+                pos_to_plddt = plddt.per_residue_plddt(start, end, r_chain, p_chain)
+
+                rows = [{"pos": pos + 1, "pLDDT": plddt} for pos, plddt in pos_to_plddt.items()]
+                df = pd.DataFrame(rows)
+
+            df["Algorithm"] = algorithm
+            df["ref"] = ref
+            dfs.append(df)
+                
+    combined_df = pd.concat(dfs, ignore_index=True)
+    combined_df.to_csv(sys.stdout, index=False)
 
 if __name__ == "__main__":
     main()
---
+++ scripts/cli.py
@@ -0,0 +1,9 @@
+import argparse
+import sys
+
+def parse_range(s):
+    try:
+        start, end = map(int, s.split('-'))
+        return start, end
+    except Exception:
+        raise argparse.ArgumentTypeError("Range must be in START-END format (e.g. 5-10)")
