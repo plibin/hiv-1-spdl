7fe5d561f22815fd8cc1e2e96a007330c7197b40
LennertSaerens <lennert.saerens@gmail.com>
2025-11-23 13:36:02 +0100

    Clarified choice of TM score computation.

--- scripts/tm.py
+++ scripts/tm.py
@@ -1,14 +1,16 @@
 import numpy as np
-from math import sqrt
 from Bio.PDB import Residue, Chain
+
 from . import core
 
-# TM-score is defined on CA atoms
+
+# TM-score is defined on CA atoms (https://doi.org/10.1093/nar/gki524 Methods Section)
 def _dist(r_ref: Residue.Residue, r_pred: Residue.Residue) -> float:
     if "CA" not in r_ref or "CA" not in r_pred:
         raise ValueError(f"Missing CA atom in residues {r_ref} or {r_pred}")
     diff = r_ref["CA"].get_coord() - r_pred["CA"].get_coord()
     return float(np.linalg.norm(diff))
+
 
 def global_tm(start: int, end: int,
               ref_chain: Chain.Chain,
@@ -23,15 +25,15 @@ def global_tm(start: int, end: int,
     # L_N is the length of the native structure,
     # L_T is the length of the aligned residues to the template structure,
     # (here that is the length of d_i_list).
-    
+
     # Handle small proteins to avoid complex numbers
     if L_N < 15:
         d_0 = 0.5
     else:
-        d_0 = 1.24 * (L_N - 15)**(1.0/3.0) - 1.8
+        d_0 = 1.24 * (L_N - 15) ** (1.0 / 3.0) - 1.8
         if d_0 < 0.5:
             d_0 = 0.5
 
-    terms = [1.0 / (1.0 + (d_i / d_0)**2) for d_i in pos_to_d_i.values()]
-    
-    return sum(terms)/float(L_N)
+    terms = [1.0 / (1.0 + (d_i / d_0) ** 2) for d_i in pos_to_d_i.values()]
+
+    return sum(terms) / float(L_N)
