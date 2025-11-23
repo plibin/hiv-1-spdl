d95d2a3b750e38ac21fe77b863a9384b251c236d
LennertSaerens <lennert.saerens@gmail.com>
2025-11-23 13:48:23 +0100

    Optimized imports and made csv path argument required.

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,18 +1,18 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 import argparse
-import pandas as pd
 from pathlib import Path
-import sys
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--csv_path", type=Path, help="Path to the CSV file")
-    parser.add_argument("--type", choices=["rmsd","plddt", "grmsd", "tm"], required=True)
+    parser.add_argument("--csv_path", type=Path, help="Path to the CSV file", required=True)
+    parser.add_argument("--type", choices=["rmsd", "plddt", "grmsd", "tm"], required=True, help="Type of plot to generate")
     args = parser.parse_args()
 
-    print (args.csv_path)
+    print(args.csv_path)
     df = pd.read_csv(args.csv_path)
     if args.type == "rmsd":
         sns.lineplot(data=df, x="pos", y="RMSD", hue="Algorithm")
@@ -22,9 +22,9 @@ def main():
         sns.boxplot(data=df, x="Algorithm", y="RMSD")
     elif args.type == "tm":
         sns.boxplot(data=df, x="Algorithm", y="TM")
-        
+
     plt.savefig(args.csv_path.stem + ".png", format="png", dpi=300)
+
 
 if __name__ == "__main__":
     main()
-
