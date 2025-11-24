418f29b870d864c3a7ad4e361dc3fa0692e28e01
LennertSaerens <lennert.saerens@gmail.com>
2025-11-24 11:30:35 +0100

    Minor refactoring and clarification regarding exclusion of algorithms.

--- scripts/config.py
+++ scripts/config.py
@@ -1,8 +1,9 @@
 def proteins():
     return ["PR", "IN", "RT"]
 
+
 def algorithms():
-    #return ["AlphaFold2", "AlphaFold3", "ESM3-Open", "ESM3-Large", "ESMFold", "Ember3D"]
-     return ["AlphaFold2", "AlphaFold3", "ESMFold"]
- # Note: Other algorithms (ESM3-Open, ESM3-Large, Ember3D) are currently excluded
- # because they appear to have incomplete coverage for Protease (PR) compared to the PDB.
+    #return ["AlphaFold2", "AlphaFold3", "ESMFold", "ESM3-Open", "ESM3-Large", "Ember3D"]
+    return ["AlphaFold2", "AlphaFold3", "ESMFold"]
+# Note: Other algorithms (ESM3-Open, ESM3-Large, Ember3D) are currently excluded
+# because they appear to have incomplete coverage for Protease (PR) compared to the PDB. --> I can't find a source for this.
