b9df17d64ddb98611e1b2aa2a0b7fc1f1c1b4015
Pieter Libin <pieter.libin@vub.be>
2026-01-09 16:36:08 +0100

    cover all algorithms

--- scripts/config.py
+++ scripts/config.py
@@ -3,7 +3,4 @@ def proteins():
 
 
 def algorithms():
-    #return ["AlphaFold2", "AlphaFold3", "ESMFold", "ESM3-Open", "ESM3-Large", "Ember3D"]
-    return ["AlphaFold2", "AlphaFold3", "ESMFold"]
-# Note: Other algorithms (ESM3-Open, ESM3-Large, Ember3D) are currently excluded
-# because they appear to have incomplete coverage for Protease (PR) compared to the PDB. --> I can't find a source for this.
+    return ["AlphaFold2", "AlphaFold3", "ESMFold", "ESM3-Open", "ESM3-Large", "Ember3D"]
