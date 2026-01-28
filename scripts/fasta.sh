c9f4f6cab4883bdc386384db42889bbdc6ed713f
Pieter Libin <pieter.libin@vub.be>
2026-01-28 21:01:03 +0100

    create an alignment (containg the query sequence and the sequence extracted from the PDB)

--- scripts/fasta.sh
+++ scripts/fasta.sh
@@ -4,4 +4,9 @@ data_dir=../data/
 
 python pdb_fasta.py --base-path $data_dir --protein RT > $data_dir/RT/refs/pdb.fasta 
 python pdb_fasta.py --base-path $data_dir --protein PR > $data_dir/PR/refs/pdb.fasta 
-python pdb_fasta.py --base-path $data_dir --protein IN > $data_dir/IN/refs/pdb.fasta 
+python pdb_fasta.py --base-path $data_dir --protein IN > $data_dir/IN/refs/pdb.fasta
+
+#align
+cat "$data_dir/RT/refs/query.fasta" "$data_dir/RT/refs/pdb.fasta" | mafft --auto - | seqkit sort > "$data_dir/RT/refs/alignment.fasta" 
+cat "$data_dir/PR/refs/query.fasta" "$data_dir/PR/refs/pdb.fasta" | mafft --auto - | seqkit sort > "$data_dir/PR/refs/alignment.fasta" 
+cat "$data_dir/IN/refs/query.fasta" "$data_dir/IN/refs/pdb.fasta" | mafft --auto - | seqkit sort > "$data_dir/IN/refs/alignment.fasta" 
