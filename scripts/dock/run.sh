ecf2a4e54207d669e80cc0715436b72bd0407579
Pieter Libin <pieter.libin@vub.be>
2026-07-03 20:57:28 +0200

    initialise the CSV files

--- scripts/dock/run.sh
+++ scripts/dock/run.sh
@@ -1,3 +1,7 @@
+echo 'name,seed,score' > AF2.docking.csv
+echo 'name,seed,score' > AF3.docking.csv
+echo 'name,seed,score' > refs.docking.csv
+
 for f in ./dock_data/refs/*.pdb; do
   name="$(basename "$f")"
   ./clean_dock.sh "./dock_data/AlphaFold2/$name" >> AF2.docking.csv 
